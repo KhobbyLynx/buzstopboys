e442e95c45843c236ea0a8fa6e36af3be7aa9a4f
Samuel Tetteh <khobbylynx55@gmail.com>
2024-12-20 14:43:05 +0000

    Update OurStats.tsx

--- src/view/about/OurStats.tsx
+++ src/view/about/OurStats.tsx
@@ -36,7 +36,7 @@ const STATS = [
 
 export function OurStats() {
   return (
-    <section className="px-8 mb-36">
+    <section className="mb-20 lg:mb-40">
       <div className="container mx-auto text-center lg:text-left">
         <div className="grid place-items-center text-center">
           <Typography variant="h2" color="blue-gray" className="mb-2 text-4xl">
