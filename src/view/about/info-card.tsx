c828ee430ca98ce47d4a25b97208bd5500de0717
Samuel Tetteh <khobbylynx55@gmail.com>
2024-12-20 14:42:59 +0000

    Update info-card.tsx

--- src/view/about/info-card.tsx
+++ src/view/about/info-card.tsx
@@ -9,8 +9,8 @@ interface InfoCardProps {
 export function InfoCard({ icon: Icon, title, children }: InfoCardProps) {
   return (
     <Card color="transparent" shadow={false}>
-      <CardBody className="grid justify-center text-center">
-        <div className="mx-auto mb-6 grid h-12 w-12 place-items-center rounded-full bg-gray-900 p-2.5 text-white shadow">
+      <CardBody className="grid justify-center text-center p-0 md:p-6 md:mx-20">
+        <div className="mx-auto mb-6 grid h-12 w-12 place-items-center rounded-full bg-gray-900 text-white shadow">
           <Icon className="h-6 w-6" strokeWidth={2} />
         </div>
         <Typography variant="h5" color="blue-gray" className="mb-2">
@@ -18,12 +18,12 @@ export function InfoCard({ icon: Icon, title, children }: InfoCardProps) {
         </Typography>
         {
           typeof(children) === 'string' ? 
-        <Typography className="px-8 font-normal !text-gray-500">
+        <Typography className="font-normal !text-gray-500">
           { children }
         </Typography> : 
         <ul>
          {Array.isArray(children) ? children.map((list, index) => (
-  <li key={index} className="px-8 font-normal !text-gray-500 ">{list}</li>
+  <li key={index} className="font-normal !text-gray-500 ">{list}</li>
 )) : null}
         </ul>
         }
