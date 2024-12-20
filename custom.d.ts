5ba5deed3361712f4b655c6a74dec807829f0443
Samuel Tetteh <khobbylynx55@gmail.com>
2024-12-20 01:34:29 +0000

    Delete custom.d.ts

--- custom.d.ts
+++
@@ -1,9 +0,0 @@
-// custom.d.ts
-import { CardProps as OriginalCardProps } from "@material-tailwind/react";
-
-declare module "@material-tailwind/react" {
-  export interface CardProps extends Omit<OriginalCardProps, 'requiredProp1' | 'requiredProp2'> {
-    requiredProp1?: string;
-    requiredProp2?: string;
-  }
-}
