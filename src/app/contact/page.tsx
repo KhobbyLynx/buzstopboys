7daff90a95085697f7cf6ac415a221c8772d02ce
Samuel Tetteh <khobbylynx55@gmail.com>
2024-12-20 14:42:24 +0000

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,15 +3,18 @@
 import Faq from "@/components/faq"
 import Team from "@/components/Team"
 import ContactForm from "@/view/Contact/ContactForm"
-import PageNavbar from "@/view/pages/PageNavbar"
+import { Box } from "@mui/material"
 
 function page() {
   return (
     <>
-        <PageNavbar />
+    <Box className="px-4 md:px-16">
         <ContactForm />
+    </Box>
         <Team />
+    <Box className="px-8 md:px-16 mb-20 lg:mb-30">
         <Faq />
+    </Box>
     </>
   )
 }
