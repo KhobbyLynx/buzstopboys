401dc756ce570f3feb228daeb6f767cc76a28deb
Samuel Tetteh <khobbylynx55@gmail.com>
2024-12-21 03:19:45 +0000

    Update page.tsx

--- src/app/(website)/activities/page.tsx
+++ src/app/(website)/activities/page.tsx
@@ -1,16 +1,15 @@
 "use client"
-
 import ActivitiesCollage from "@/view/Activiteis/ActivitiesCollage"
 import Highlight from "@/view/Activiteis/Highlight"
-import VideoReel from "@/view/Activiteis/VideoReel"	
 import { Box } from "@mui/material"
+import VideoSwiper from "@/components/Carousel/VideoSwiper"
 
 function page() {
   return (
     <>
     <Box className="px-8">
       <ActivitiesCollage />
-      <VideoReel />
+        <VideoSwiper />
     </Box>
         <Highlight />
     </>
