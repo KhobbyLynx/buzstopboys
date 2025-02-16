cffab6b71166798e7e334acf690ba6554b1f6388
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-16 10:37:01 +0000

    apis

--- src/app/api/patrons/route.ts
+++ src/app/api/patrons/route.ts
@@ -1,6 +1,8 @@
+import { adminDeleteFirebaseUser, adminReinstateFirebaseUser, adminSuspendFirebaseUser } from "@/configs/adminFirebase";
 import connectMongoDB from "@/libs/mongodb";
 import Patron from "@/models/patron.model";
 import { generateRandomId } from "@/utils/utils";
+import { getAuth } from "firebase/auth";
 import { NextRequest, NextResponse } from "next/server";
 
 const DEMOPATRONS = [
@@ -34,17 +36,43 @@ const DEMOPATRONS = [
 ];
 
 
-//** GET */
+//** GET All or Single PATRON */
 export async function GET(request: NextRequest) {
     try {
         // Connect to MongoDB
         await connectMongoDB();
 
-        // Fetch all patrons
+        // Get the ID from the request query (if provided)
+        const { searchParams } = new URL(request.url);
+        const id = searchParams.get("id");
+
+        if (id) {
+            // Fetch a single patron by ID
+            const patron = await Patron.findOne({ id });
+
+            if (!patron) {
+                return new Response(JSON.stringify({ message: "Patron not found" }), {
+                    status: 404,
+                    headers: {
+                        "content-type": "application/json",
+                    },
+                });
+            }
+
+            // Return the single patron
+            return new Response(JSON.stringify(patron), {
+                headers: {
+                    "content-type": "application/json",
+                },
+                status: 200,
+            });
+        }
+
+        // Fetch all patrons if no ID is provided
         const patrons = await Patron.find();
 
-        // Return patrons
-        return new Response(JSON.stringify(DEMOPATRONS), {
+        // Return all patrons
+        return new Response(JSON.stringify(patrons), {
             headers: {
                 "content-type": "application/json",
             },
@@ -62,6 +90,7 @@ export async function GET(request: NextRequest) {
     }
 }
 
+
 //** POST - Create Patron */
 export async function POST(request: NextRequest) {
     try {
@@ -71,34 +100,23 @@ export async function POST(request: NextRequest) {
         // Get the patron data from the request body
         const patronData = await request.json(); 
 
-        const  {username, firstname, lastname, contact, email, role, avatar, address} = patronData
+        const  {id, username, email, role} = patronData
     
         console.log("CREATE NEW PATRON", patronData);
         // Check if the required fields are provided
-        if (!username || !email || !role) {
+        if (!id || !username || !email || !role) {
             return NextResponse.json({ message: "Patron Name and Email are required" }, { status: 400 });
         }
 
-        // Generate random id
-        const id = generateRandomId()
-        
         // Create a new patron
-        await Patron.create({
-            id,
-            username,
-            email,
-            address,
-            firstname,
-            lastname,
+        const newPatron = await Patron.create({
+            ...patronData,
             onlineStatus: false,
             verified: false,
-            contact,
-            role,
-            avatar,
         });
 
         // Return a success response
-        return NextResponse.json({message: `New Patron with id ${id} Created`}, {
+        return NextResponse.json(newPatron, {
           headers: {
             "content-type": "application/json",
           },
@@ -115,48 +133,49 @@ export async function POST(request: NextRequest) {
 //** DELETE  */
 export async function DELETE(request: NextRequest) {
     try {
-        // Get the patron ID from the body
-        const id = request.body 
+         // Connect to MongoDB
+         await connectMongoDB();
+         
+        const id = request.nextUrl.searchParams.get('id');
 
-        console.log('Patron ID', id)
         // Check if the patron ID is provided
         if (!id) {
             return NextResponse.json({ message: "Patron ID is required" }, { status: 400 });
         }
+    
+        // Delete the patron from Firebase
+        adminDeleteFirebaseUser(id);
+        
+        // Delete the patron from MongoDB
+        const deletedPatron = await Patron.findOneAndDelete({ id });
 
-        // Connect to MongoDB
-        await connectMongoDB();
+        if (!deletedPatron) {
+            return NextResponse.json({ message: "Patron not found" }, { status: 404 });
+        }
 
-        // Delete the patron
-        await Patron.findOneAndDelete({ id });
-
-        // Return a success response
-        return NextResponse.json({message: "Patron Deleted"}, {
-          headers: {
-            "content-type": "application/json",
-          },
-          status: 200,
-        });
+        return NextResponse.json({ message: "Patron Deleted" }, { status: 200 });
     } catch (error: any) {
         console.error("Error deleting patron:", error);
-        return new Response(JSON.stringify({ message: error.message }), {
-            status: 500
-        });
+        return NextResponse.json({ message: error.message }, { status: 500 });
     }
 }
+
 
 //** PUT  */
 export async function PUT(request: NextRequest) {
     try {
-        // Get the patron ID from the body
-        const id = request.json()
-        // Check if the patron ID is provided
-        if (!id) {
-            return NextResponse.json({ message: "Patron ID is required" }, { status: 400 });
-        }
 
-        // Validate the request body
-        const { firstname, lastname, contact, role } = await request.json();
+        // Get the ID from the request query (if provided)
+        const { searchParams } = new URL(request.url);
+        const id = searchParams.get("id");
+
+            // Check if the patron ID is provided
+            if (!id) {
+                return NextResponse.json({ message: "Patron ID is required" }, { status: 400 });
+            }
+           
+            // Validate the request body
+            const { firstname, lastname, contact, role, lastSignInTime, onlineStatus } = await request.json();
 
         // Connect to MongoDB
         await connectMongoDB();
@@ -164,13 +183,62 @@ export async function PUT(request: NextRequest) {
         // Update the patron
         const patron = await Patron.findOneAndUpdate(
             { id },
-            { $set: { firstname, lastname, contact, role } },
+            { $set: { firstname, lastname, contact, role, lastSignInTime, onlineStatus } },
             { new: true } // return the updated document
         );
         console.log(patron);
 
         // Return a success response
-        return NextResponse.json({message: "Patron Updated"}, {
+        return NextResponse.json(patron, {
+          headers: {
+            "content-type": "application/json",
+          },
+          status: 200,
+        });
+    } catch (error: any) {
+        console.error("Error updating patron:", error);
+        return new Response(JSON.stringify({ message: error.message }), {
+            status: 500
+        });
+    }
+}
+
+
+//** PATCH  */
+export async function PATCH(request: NextRequest) {
+    try {
+
+        // Get the ID from the request query (if provided)
+        const { searchParams } = new URL(request.url);
+        const id = searchParams.get("id");
+
+            // Check if the patron ID is provided
+            if (!id) {
+                return NextResponse.json({ message: "Patron ID is required" }, { status: 400 });
+            }
+           
+            // Validate the request body
+            const { suspended } = await request.json();
+
+        // Connect to MongoDB
+        await connectMongoDB();
+
+        if(suspended){
+            await adminSuspendFirebaseUser(id) 
+        } else {
+            await adminReinstateFirebaseUser(id)
+        }
+
+        // Update the patron
+        const patron = await Patron.findOneAndUpdate(
+            { id },
+            { $set: { suspended } },
+            { new: true } // return the updated document
+        );
+        console.log(patron);
+
+        // Return a success response
+        return NextResponse.json(patron, {
           headers: {
             "content-type": "application/json",
           },
