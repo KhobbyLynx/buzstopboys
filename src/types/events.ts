c8d691bc6030600e8d3e9d7fad5057c14d97fb2c
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-22 03:53:26 +0000

    update event types

--- src/types/events.ts
+++ src/types/events.ts
@@ -1,42 +1,45 @@
 export type EventProps = {
-    _id: string; // MongoDB ObjectId as a string
-    id: string; // Activity ID  
-    title: string;
-    desc: string;
-    img: string;
-    hashTags: string[];
-    startDate: string;
-    endDate: string;
-    startTime: string;
-    endTime: string;
-    caption: string;
-    status: 'upcoming' | 'past' | 'suspended'
-    createdAt: string; // ISO date string
-    updatedAt: string; // ISO date string
-    __v: number; // Version key
+  _id: string // MongoDB ObjectId as a string
+  id: string // Activity ID
+  title: string
+  desc: string
+  img: string
+  hashTags: string[]
+  startDate: string
+  endDate: string
+  startTime: string
+  endTime: string
+  caption: string
+  venue: string
+  status: 'upcoming' | 'past' | 'suspended'
+  createdAt: string // ISO date string
+  updatedAt: string // ISO date string
+  __v: number // Version key
 }
 
 export type AddEventType = {
-    title: string
-    desc: string
-    hashTags: string[]
-    startDate: string;
-    endDate: string;
-    startTime: string;
-    endTime: string;
-    caption: string
-    venue: string
-    status: 'upcoming' | 'past' | 'suspended'
+  title: string
+  desc: string
+  hashTags: string[]
+  startDate: string
+  endDate: string
+  startTime: string
+  endTime: string
+  caption: string
+  venue: string
+  img: string | null
+  status: 'upcoming' | 'past' | 'suspended'
 }
 export type EditEventType = {
-    title: string
-    desc: string
-    hashTags: string[]
-    startDate: string;
-    endDate: string;
-    startTime: string;
-    endTime: string;
-    caption: string
-    venue: string
-    status: 'upcoming' | 'past' | 'suspended'
+  title: string
+  desc: string
+  hashTags: string[]
+  startDate: string
+  endDate: string
+  startTime: string
+  endTime: string
+  caption: string
+  venue: string
+  img: string | null
+  status: 'upcoming' | 'past' | 'suspended'
 }
