5b80d0f7defdba6bb217802a55f4da1784f80e7e
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-22 03:52:32 +0000

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,36 +1,42 @@
-import { NextResponse, NextRequest } from "next/server";
-import { parse } from "cookie";
+import { NextResponse, NextRequest } from 'next/server'
+import { parse } from 'cookie'
 
 export async function middleware(request: NextRequest) {
-  console.log("Middleware running");
-
   // Access cookies from headers
-  const cookieHeader = request.headers.get("cookie") || "";
-  const cookies = parse(cookieHeader);
+  const cookieHeader = request.headers.get('cookie') || ''
+  const cookies = parse(cookieHeader)
 
-  const userData = cookies.userData ? JSON.parse(cookies.userData) : null;
+  const userData = cookies.userData ? JSON.parse(cookies.userData) : null
 
   if (!userData) {
-    console.log("No user is logged in @ Middleware");
-    return NextResponse.redirect(new URL("/", request.url));
+    if (process.env.NODE_ENV === 'development') {
+      console.log('No user is logged in @ Middleware')
+    }
+    return NextResponse.redirect(new URL('/', request.url))
   }
 
   try {
-    const isAdmin = userData.role === "admin";
-
+    const isAdmin = userData.role === 'admin'
     if (!isAdmin) {
-      console.log("User is not an admin");
-      return NextResponse.redirect(new URL("/", request.url));
+      if (process.env.NODE_ENV === 'development') {
+        console.log('User is not an admin @ Middleware')
+      }
+
+      return NextResponse.redirect(new URL('/', request.url))
     }
   } catch (error) {
-    console.error("Error fetching patron info:", error);
-    return NextResponse.redirect(new URL("/", request.url));
+    if (process.env.NODE_ENV === 'development') {
+      console.log('Error fetching patron info:', error)
+    }
+    return NextResponse.redirect(new URL('/', request.url))
   }
 
-  console.log("User is logged in and is an admin");
-  return NextResponse.next();
+  if (process.env.NODE_ENV === 'development') {
+    console.log('User is logged in and is an admin @ Middleware')
+  }
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ["/admin/:path*"],
-};
+  matcher: ['/admin/:path*'],
+}
