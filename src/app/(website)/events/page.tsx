31ccaa7ad10f6776386a7f9a2cb1389455eca69b
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-22 03:45:36 +0000

    events page loader

--- src/app/(website)/events/page.tsx
+++ src/app/(website)/events/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import { AppDispatch, RootState } from '@/store';
-import { getEvents } from '@/store/events';
+import { AppDispatch, RootState } from '@/store'
+import { getEvents } from '@/store/events'
 import EventCard from '@/view/events/EventsCard'
 import EventsHeader from '@/view/events/EventsHeader'
-import { Typography } from '@material-tailwind/react'; 
+import { Typography } from '@material-tailwind/react'
+import { Box, CircularProgress } from '@mui/material'
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux'
 
 // const eventData = [
 //     {
@@ -46,35 +47,39 @@ import { useDispatch, useSelector } from 'react-redux';
 //       img: 'https://res.cloudinary.com/khobbylynx/image/upload/v1739193353/buzstopboys/info_graphics/GiX_eOkWYAA0iec_e8dkan.jpg'
 //     }
 //   ];
-  
+
 const Events = () => {
   const dispatch = useDispatch<AppDispatch>()
-  const store = useSelector((state : RootState) => state.events)
+  const store = useSelector((state: RootState) => state.events)
+  const { pending, events } = store
 
-  const eventData = store.events
-  
-  useEffect(()=> {
-    dispatch(getEvents())
-  }, [ dispatch ])
+  useEffect(() => {
+    if (events.length === 0) dispatch(getEvents())
+  }, [dispatch])
+
+  if (pending) {
+    return (
+      <Box sx={{ mt: 6, display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+        <CircularProgress sx={{ mb: 4 }} />
+        <Typography>Loading...</Typography>
+      </Box>
+    )
+  }
 
   return (
     <div>
-        <EventsHeader />
+      <EventsHeader />
 
-    <div className='bg-gray-100 p-10'>
+      <div className="bg-gray-100 p-10">
         <Typography variant="h4" className="my-3 text-gray-900 text-center uppercase">
           Upcoming Events
         </Typography>
-        <div className='flex align-middle justify-center'>
-        {
-            eventData.map(event => {
-                return(
-                <EventCard key={event.title}  {...event}/>
-                )
-            })
-        }
+        <div className="flex items-start justify-between w-full flex-wrap">
+          {events.map((event) => {
+            return <EventCard key={event.title} {...event} />
+          })}
         </div>
-    </div>
+      </div>
     </div>
   )
 }
