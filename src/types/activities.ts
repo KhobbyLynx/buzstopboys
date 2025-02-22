c15c30dbb1eea5942b3441c756e54cd0459671dd
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-22 03:53:13 +0000

    Update activities.ts

--- src/types/activities.ts
+++ src/types/activities.ts
@@ -1,34 +1,30 @@
 export type ActivityProps = {
-    _id: string; // MongoDB ObjectId as a string
-    id: string; // Activity ID  
-    title: string;
-    desc: string;
-    imgs: string[];
-    videoUrls: string[];
-    details: string[];
-    caption: string;
-    icon: string;
-    createdAt: string; // ISO date string
-    updatedAt: string; // ISO date string
-    __v: number; // Version key
+  _id: string // MongoDB ObjectId as a string
+  id: string // Activity ID
+  title: string
+  desc: string
+  caption: string
+  imgs: string[]
+  videoUrls: string[]
+  details: string[]
+  icon: string
+  createdAt: string // ISO date string
+  updatedAt: string // ISO date string
+  __v: number // Version key
 }
 
 export type EditActivityType = {
-    id: string; // Activity ID
-    title: string;
-    desc: string;
-    imgs: string[];
-    videoUrls: string[];
-    details: string[];
-    caption: string;
-    icon: string
+  title: string
+  desc: string
+  caption: string
 }
 
 export type AddActivityType = {
-    title: string
-    desc: string
-    videoUrls: string[]
-    details?: string[]
-    caption?: string
-    icon: string
+  title: string
+  desc: string
+  videoUrls: string[]
+  details: string[]
+  caption: string
+  icon: string
+  imgs: string[]
 }
