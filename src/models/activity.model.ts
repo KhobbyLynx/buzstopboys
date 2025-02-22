70951ebd47bf42988eceacf6ebd569ba4b99b4c6
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-22 03:52:21 +0000

    Update activity.model.ts

--- src/models/activity.model.ts
+++ src/models/activity.model.ts
@@ -1,42 +1,43 @@
 import mongoose, { Schema } from 'mongoose'
 
 const activitySchema = new Schema(
-	{
-		id: {
-			type: String,
-			required: true
-		},
-		title: {
-			type: String,
-			required: true,
-			trim: true
-		},
-		desc: {
-			type: String,
-			required: true,
-			trim: true
-		},
-		imgs: {
-			type: [String],
-		},
-		videoUrls: {
-			type: [String],
-			required: true
-		},
-		icon: {
-			type: String,
-			required: true
-		},
-		caption: {
-			type: String,
-		},
-		details: {
-			type: [String],
-		}
-	},
-	{
-		timestamps: true
-	}
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    imgs: {
+      type: [String],
+      required: true,
+    },
+    videoUrls: {
+      type: [String],
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    caption: {
+      type: String,
+    },
+    details: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
 )
 
 const Activity = mongoose.models.Activity || mongoose.model('Activity', activitySchema)
