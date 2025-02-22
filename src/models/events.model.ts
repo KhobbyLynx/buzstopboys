c559d711f7c0a253f10626ea996764a687f2526a
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-22 03:52:23 +0000

    Update events.model.ts

--- src/models/events.model.ts
+++ src/models/events.model.ts
@@ -1,56 +1,57 @@
 import mongoose, { Schema } from 'mongoose'
 
 const eventSchema = new Schema(
-    {
-        id: {
-            type: String,
-            required: true
-        },
-        title: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        desc: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        img: {
-            type: String,
-        },
-        startDate: {
-            type: String,
-        },
-        endDate: {
-            type: String,
-        },
-        startTime: {
-            type: String,
-        },
-        endTime: {
-            type: String,
-        },
-        venue: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        caption: {
-            type: String,
-        },
-        status: {
-            type: String,
-            required: true
-        },
-        hashTags: {
-            type: [String],
-            required: true
-        }
+  {
+    id: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    img: {
+      type: String,
+    },
+    startDate: {
+      type: String,
+      required: true,
+    },
+    endDate: {
+      type: String,
+    },
+    startTime: {
+      type: String,
+    },
+    endTime: {
+      type: String,
+    },
+    venue: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    caption: {
+      type: String,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    hashTags: {
+      type: [String],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 )
 
 const Event = mongoose.models.Event || mongoose.model('Event', eventSchema)
