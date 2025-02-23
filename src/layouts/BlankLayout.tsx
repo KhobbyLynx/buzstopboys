b9c7f80e924f81be15ad27ab08b8d41ba0bda2a8
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-23 04:15:17 +0000

    Update BlankLayout.tsx

--- src/layouts/BlankLayout.tsx
+++ src/layouts/BlankLayout.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React from 'react'
+import { Box } from '@mui/material'
 
 const BlankLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return (
-    <Box component="main">
-      {children}
-    </Box>
-  );
-};
+  return <Box component="main">{children}</Box>
+}
 
-export default BlankLayout;
+export default BlankLayout
