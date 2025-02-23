8967d616c7835f3f3833342dbc32179ce269db6f
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-23 04:15:23 +0000

    Update MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,24 +1,24 @@
-"use client"
-import React from 'react';
-import { Box } from '@mui/material';
-import { usePathname } from 'next/navigation';
-import { Footer, Navbar } from '@/components';
-import PageNavbar from '@/view/pages/PageNavbar';
+'use client'
+import React from 'react'
+import { Box } from '@mui/material'
+import { usePathname } from 'next/navigation'
+import { Footer, Navbar } from '@/components'
+import PageNavbar from '@/view/pages/PageNavbar'
 
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-   const pathname = usePathname();
+  const pathname = usePathname()
   return (
     <Box>
-        {/* Different Navbar for Homepage and the rest of the pages */}
-      {pathname === '/'? <Navbar/> : <PageNavbar />}
+      {/* Different Navbar for Homepage and the rest of the pages */}
+      {pathname === '/' ? <Navbar /> : <PageNavbar />}
 
-        {/* Main Content */}
+      {/* Main Content */}
       <Box component="main">{children}</Box>
 
-        {/* Footer */}
+      {/* Footer */}
       <Footer />
     </Box>
-  );
-};
+  )
+}
 
-export default MainLayout;
+export default MainLayout
