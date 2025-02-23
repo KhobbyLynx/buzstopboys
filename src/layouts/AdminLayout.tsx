e8b30d324ed7488c210054174fd5dd4cbf9b4ad9
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-23 04:15:13 +0000

    Update AdminLayout.tsx

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,28 +1,26 @@
-import React from "react";
+import React from 'react'
 
-// Components 
-import AdminNavbar from "@/components/Navbars/AdminNavbar";
-import Sidebar from "@/components/Sidebar/Sidebar";
-import HeaderStats from "@/components/Headers/HeaderStats";
-import FooterAdmin from "@/components/Footers/FooterAdmin";
+// Components
+import AdminNavbar from '@/components/navbars/AdminNavbar'
+import Sidebar from '@/components/sidebar/Sidebar'
+import HeaderStats from '@/components/headers/HeaderStats'
+import FooterAdmin from '@/components/footers/FooterAdmin'
 
 const AdminLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    return (
-        <>
-          <Sidebar />
-          <div className="relative md:ml-64 bg-blueGray-100 min-h-screen flex flex-col">
-            {/* <AdminNavbar /> */}
-            {/* Header */}
-            {/* <HeaderStats /> */}
-            <div className="px-4 md:px-10 mx-auto w-full flex-grow">
-              {children}
-            </div>
-            <div className="mt-auto">
-              <FooterAdmin />
-            </div>
-          </div>
-        </>
-      );
-};
+  return (
+    <>
+      <Sidebar />
+      <div className="relative md:ml-64 bg-blueGray-100 min-h-screen flex flex-col">
+        {/* <AdminNavbar /> */}
+        {/* Header */}
+        {/* <HeaderStats /> */}
+        <div className="px-4 md:px-10 mx-auto w-full flex-grow">{children}</div>
+        <div className="mt-auto">
+          <FooterAdmin />
+        </div>
+      </div>
+    </>
+  )
+}
 
-export default AdminLayout;
+export default AdminLayout
