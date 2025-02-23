b037c735d62f40de2ba2f7f2db5e929f288cf988
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-23 04:15:20 +0000

    Update index.tsx

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,14 +1,15 @@
 'use client'
-import React, { useEffect } from 'react';
-import { usePathname } from 'next/navigation';
-import MainLayout from './MainLayout';
-import BlankLayout from './BlankLayout';
-import AdminLayout from './AdminLayout';
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '@/configs/firebase';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '@/store';
-import { handleAutoLogin } from '@/store/auth';
+import React, { useEffect } from 'react'
+import { usePathname } from 'next/navigation'
+import MainLayout from './MainLayout'
+import BlankLayout from './BlankLayout'
+import AdminLayout from './AdminLayout'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '@/configs/firebase'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppDispatch, RootState } from '@/store'
+import { handleAutoLogin } from '@/store/auth'
+import FallbackSpinner from '@/components/spinner'
 
 interface DataType {
   id: string
@@ -19,49 +20,65 @@ interface DataType {
 }
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const dispatch = useDispatch<AppDispatch>()
+  const dispatch = useDispatch<AppDispatch>()
+  const store = useSelector((state: RootState) => state.auth)
+  const { pending } = store
 
-    // ** useEfects
-    useEffect(() => {
-      // ** onAuthStateChange
-      const unsubscribe = onAuthStateChanged(auth,async (user) => {
-        if (user) {
-          try {
+  // ** useEfects
+  useEffect(() => {
+    // ** onAuthStateChange
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        try {
+          if (process.env.NODE_ENV === 'development') {
             console.log('User is signed in:', user)
-            
-            dispatch(handleAutoLogin())
-          } catch (error) {
-            console.error("Error getting access token:", error);
           }
-        } else {
+
+          dispatch(handleAutoLogin())
+        } catch (error) {
+          if (process.env.NODE_ENV === 'development') {
+            console.error('Error getting access token:', error)
+          }
+        }
+      } else {
+        if (process.env.NODE_ENV === 'development') {
           console.log('User is signed out')
         }
-      })
- 
-      // ** Cleaner Func.
-      return () => {
-        unsubscribe()
       }
-    }, [dispatch])
-    
+    })
 
-    const pathname = usePathname();
-    const isBlankLayout = ['/sign-up', '/log-in','/forgot-password', '/not-found'].includes(pathname);
-    const isAdminLayout = pathname.startsWith('/admin');
-    const isMainLayout = ['/', '/our-merch', '/activities', '/donate', '/about-us', '/contact', ].includes(pathname);
-    
+    // ** Cleaner Func.
+    return () => {
+      unsubscribe()
+    }
+  }, [dispatch])
+
+  const pathname = usePathname()
+  const isBlankLayout = ['/sign-up', '/log-in', '/forgot-password', '/not-found'].includes(pathname)
+  const isAdminLayout = pathname.startsWith('/admin')
+  const isMainLayout = [
+    '/',
+    '/our-merch',
+    '/activities',
+    '/donate',
+    '/about-us',
+    '/contact',
+  ].includes(pathname)
+
+  if (pending) {
+    return <FallbackSpinner />
+  }
   return (
     <>
-        {isBlankLayout ? (
-           <BlankLayout>{children}</BlankLayout>
-         ) : isAdminLayout ?  (
-           <AdminLayout>{children}</AdminLayout>
-        ) : (
-          <MainLayout>{children}</MainLayout>
-        )
-        }
+      {isBlankLayout ? (
+        <BlankLayout>{children}</BlankLayout>
+      ) : isAdminLayout ? (
+        <AdminLayout>{children}</AdminLayout>
+      ) : (
+        <MainLayout>{children}</MainLayout>
+      )}
     </>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
