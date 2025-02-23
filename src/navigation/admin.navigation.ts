a20721ba099240a350fedc0cad33771ab02b006b
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-23 04:15:29 +0000

    Update admin.navigation.ts

--- src/navigation/admin.navigation.ts
+++ src/navigation/admin.navigation.ts
@@ -1,15 +1,18 @@
-import { NavigationType } from "./type";
+import { NavigationType } from './type'
 
 export const adminNavigation: NavigationType[] = [
-    { name: "Dashboard", href: "/admin/dashboard", icon: "fa-border-all" },
-    { name: "Patrons", href: "/admin/patrons", icon: "fa-users" },
-    { name: "Donations", href: "/admin/donations", icon: "fa-hand-holding-dollar" },
-    { name: "Activities", href: "/admin/activities", icon: "fa-person-digging" },
-    { name: "Events", href: "/admin/events", icon: "fa-calendar-days" },
-    { name: "Reviews", href: "/admin/reviews", icon: "fa-comment-dots" },
-    { name: "Our Merch", href: "/admin/products", icon: "fa-bag-shopping" },
-    { name: "Orders", href: "/admin/orders", icon: "fa-cart-shopping" },
-    { name: "Invoice", href: "/admin/invoice", icon: "fa-receipt" },
-    { name: "Refund", href: "/admin/refunds", icon: "fa-filter-circle-dollar" },
-    { name: "Site Settings", href: "/admin/site-settings", icon: "fa-gear" },
-  ];
+  { name: 'Dashboard', href: '/admin/dashboard', icon: 'fa-border-all' },
+  { name: 'Patrons', href: '/admin/patrons', icon: 'fa-users' },
+  { name: 'Donations', href: '/admin/donations', icon: 'fa-hand-holding-dollar' },
+  { name: 'Activities', href: '/admin/activities', icon: 'fa-person-digging' },
+  { name: 'Events', href: '/admin/events', icon: 'fa-calendar-days' },
+  { name: 'Volunteers', href: '/admin/volunteers', icon: 'fa-handshake-angle' },
+  { name: 'Inbox', href: '/admin/inbox', icon: 'fa-message' },
+  // { name: "Our Merch", href: "/admin/products", icon: "fa-bag-shopping" },
+  // { name: "Orders", href: "/admin/orders", icon: "fa-cart-shopping" },
+  // { name: "Invoice", href: "/admin/invoice", icon: "fa-receipt" },
+  // { name: "Refund", href: "/admin/refunds", icon: "fa-filter-circle-dollar" },
+  // { name: 'Reviews', href: '/admin/reviews', icon: 'fa-comment-dots' },
+  // { name: "Site Settings", href: "/admin/site-settings", icon: "fa-gear" },
+]
+// <i class="fa-solid fa-handshake-angle"></i>
