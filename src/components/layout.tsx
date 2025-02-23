11b03676431f878a582dd81b9a129bd7340f8512
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-23 04:14:38 +0000

    Update layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,18 +1,31 @@
 "use client";
 
+// ** React imports
 import React from "react";
-import { ThemeProvider } from "@material-tailwind/react";
-import { Provider } from "react-redux";
-import {  store } from "@/store";
 
+// ** Store
+import { Provider } from "react-redux";
+import { store } from "@/store";
+
+// ** Loader Import
+import { AppProgressProvider as ProgressProvider } from '@bprogress/next';
+
+import { ThemeProvider } from "@material-tailwind/react";
 
 export function ThemeProviderLayout({ children }: { children: React.ReactNode }) {
+
   return (
-  <Provider store={store}>
-  {/* // <ThemeProvider>{children}</ThemeProvider> */}
-    {children}
-  </Provider>
-  )
+    <Provider store={store}>
+      <ProgressProvider
+       color="#060723"
+       options={{ showSpinner: false }}
+       shallowRouting
+       >
+        {/* // <ThemeProvider>{children}</ThemeProvider> */}
+        {children}
+      </ProgressProvider>
+    </Provider>
+  );
 }
 
 export default ThemeProviderLayout;
