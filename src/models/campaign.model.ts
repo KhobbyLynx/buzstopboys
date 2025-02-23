975a5a2fe7b5499b884eb929623570f0f9b7d164
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-23 04:15:26 +0000

    Update campaign.model.ts

--- src/models/campaign.model.ts
+++ src/models/campaign.model.ts
@@ -1,42 +1,50 @@
 import mongoose, { Schema } from 'mongoose'
 
-const campaignSchema = new Schema({
+const campaignSchema = new Schema(
+  {
     id: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     target: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     raised: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     title: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     desc: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     subText: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true,
     },
     status: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    img: {
-        type: String,
-    }
-}, {
-    timestamps: true
-})
+    imgs: {
+      type: [String],
+      required: true,
+    },
+    details: {
+      type: [String],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
 
 const Campaign = mongoose.models.Campaign || mongoose.model('Campaign', campaignSchema)
 export default Campaign
