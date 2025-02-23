11dd582813a3e9df8a9fd47663a4e114b7832b19
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-23 04:16:42 +0000

    Update TableHeader.tsx

--- src/view/admin/users/list/TableHeader.tsx
+++ src/view/admin/users/list/TableHeader.tsx
@@ -4,6 +4,10 @@ import Button from '@mui/material/Button'
 
 // ** Icon Imports
 import Icon from '@/components/icon'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { AppDispatch } from '@/store'
+import { getPatrons } from '@/store/users'
 
 interface TableHeaderProps {
   toggle: () => void
@@ -13,6 +17,17 @@ const TableHeader = (props: TableHeaderProps) => {
   // ** Props
   const { toggle } = props
 
+  // ** State
+  const [refreshing, setRefreshing] = useState<boolean>(false)
+
+  const dispatch = useDispatch<AppDispatch>()
+
+  const handleRefresh = () => {
+    setRefreshing(true)
+    dispatch(getPatrons()).then(() => {
+      setRefreshing(false)
+    })
+  }
   return (
     <Box
       sx={{
@@ -23,15 +38,21 @@ const TableHeader = (props: TableHeaderProps) => {
         display: 'flex',
         flexWrap: 'wrap',
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
       }}
     >
-      <Button color='primary' variant='outlined' startIcon={<Icon icon='tabler:upload' />}>
-        Export
+      <Button
+        onClick={handleRefresh}
+        color="primary"
+        variant="outlined"
+        disabled={refreshing}
+        startIcon={<Icon icon={refreshing ? 'tabler:loader' : 'tabler:refresh'} />}
+      >
+        {refreshing ? 'Refreshing...' : 'Refresh'}
       </Button>
       <Box sx={{ rowGap: 2, display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
-        <Button onClick={toggle} variant='contained' sx={{ '& svg': { mr: 2 } }}>
-          <Icon fontSize='1.125rem' icon='tabler:plus' />
+        <Button onClick={toggle} variant="contained" sx={{ '& svg': { mr: 2 } }}>
+          <Icon fontSize="1.125rem" icon="tabler:plus" />
           Add New Patron
         </Button>
       </Box>
