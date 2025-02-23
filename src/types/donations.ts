ed699835ce437ed60e20f010cfa5f680bab28ccf
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-23 04:16:05 +0000

    Update donations.ts

--- src/types/donations.ts
+++ src/types/donations.ts
@@ -1,39 +1,42 @@
 type CampaignStatus = 'active' | 'suspended' | 'completed'
 
 export type DonationCampaignProps = {
-    _id: string; // MongoDB ObjectId as a string
-    id: string; // Campaign ID
-    title: string; 
-    desc: string;
-    img: string; 
-    raised: number; 
-    target: number; 
-    status: CampaignStatus;
-    createdAt: string; // ISO date string
-    updatedAt: string; // ISO date string
-    __v: number; // Version key
+  _id: string // MongoDB ObjectId as a string
+  id: string // Campaign ID
+  title: string
+  desc: string
+  img: string
+  raised: number
+  target: number
+  details: string[]
+  imgs: string[]
+  subText: string
+  status: CampaignStatus
+  createdAt: string // ISO date string
+  updatedAt: string // ISO date string
+  __v: number // Version key
 }
 
-export type DonationOptionsProps = { 
-    _id?: string; // MongoDB ObjectId as a string
-    id: string; // Donation option ID
-    name: string;
-    amount: number; 
-    amountRaised: number;
-    numberOfDonors: number;
-    createdAt: string; // ISO date string
-    updatedAt: string; // ISO date string
-    __v: number; // Version key
+export type DonationOptionsProps = {
+  _id?: string // MongoDB ObjectId as a string
+  id: string // Donation option ID
+  name: string
+  amount: number
+  amountRaised: number
+  numberOfDonors: number
+  createdAt: string // ISO date string
+  updatedAt: string // ISO date string
+  __v: number // Version key
 }
 
-export type DonationOptionType =  {
-    name?: string
-    amount?: number
+export type DonationOptionType = {
+  name: string
+  amount: number
 }
 
-export type EditDonationOptionType =  {
-    name?: string
-    amount?: number
-    numberOfDonors?: number
-    amountRaised?: number
+export type EditDonationOptionType = {
+  name: string
+  amount: number
+  numberOfDonors?: number
+  amountRaised?: number
 }
