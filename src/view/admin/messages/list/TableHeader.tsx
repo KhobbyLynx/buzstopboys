016d8583a2f90d7b8c79d986de197c0fba231452
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-25 08:08:33 +0000

    Create TableHeader.tsx

---
+++ src/view/admin/messages/list/TableHeader.tsx
@@ -0,0 +1,51 @@
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+
+// ** Icon Imports
+import Icon from '@/components/icon'
+
+interface TableHeaderProps {
+  toggle: () => void
+  handleRefresh: () => void
+  refreshing: boolean
+  sending: boolean
+}
+
+const TableHeader = (props: TableHeaderProps) => {
+  // ** Props
+  const { toggle, handleRefresh, refreshing, sending } = props
+
+  return (
+    <Box
+      sx={{
+        py: 4,
+        px: 6,
+        rowGap: 2,
+        columnGap: 4,
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Button
+        onClick={handleRefresh}
+        color="primary"
+        variant="outlined"
+        disabled={refreshing}
+        startIcon={<Icon icon={refreshing ? 'tabler:loader' : 'tabler:refresh'} />}
+      >
+        {refreshing ? 'Refreshing...' : 'Refresh'}
+      </Button>
+      <Box sx={{ rowGap: 2, display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
+        <Button onClick={toggle} variant="contained" sx={{ '& svg': { mr: 2 } }}>
+          <Icon fontSize="1.125rem" icon={sending ? 'tabler:loader' : 'mynaui:send-solid'} />
+          Send Message
+        </Button>
+      </Box>
+    </Box>
+  )
+}
+
+export default TableHeader
