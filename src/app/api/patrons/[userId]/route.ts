ced46c3b64478b633b409994f04f13b51511ff49
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-25 08:08:17 +0000

    Create route.ts

---
+++ src/app/api/patrons/[userId]/route.ts
@@ -0,0 +1,33 @@
+import connectMongoDB from '@/libs/mongodb'
+import Patron from '@/models/patron.model'
+import { NextRequest } from 'next/server'
+
+// ** GET
+export async function GET(request: NextRequest, context: { params: Promise<{ userId: string }> }) {
+  try {
+    const { userId } = await context.params // Await params
+    // Connect to MongoDB
+    await connectMongoDB()
+
+    // Fetch by ID (Single Patron)
+    const patron = await Patron.findOne({ id: userId }, { _id: 0, __v: 0 })
+
+    if (!patron) {
+      return new Response(JSON.stringify({ message: 'Patron not found' }), {
+        status: 404,
+        headers: { 'content-type': 'application/json' },
+      })
+    }
+
+    return new Response(JSON.stringify(patron), {
+      status: 200,
+      headers: { 'content-type': 'application/json' },
+    })
+  } catch (error: any) {
+    console.error('Error fetching patron by ID:', error)
+    return new Response(JSON.stringify({ message: error.message }), {
+      status: 500,
+      headers: { 'content-type': 'application/json' },
+    })
+  }
+}
