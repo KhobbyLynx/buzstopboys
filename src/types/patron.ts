1a31cf32d15302d7f3d6766b839128bc0b99bbf6
Samuel Tetteh <khobbylynx55@gmail.com>
2025-02-26 10:08:07 +0000

    Update patron.ts

--- src/types/patron.ts
+++ src/types/patron.ts
@@ -1,73 +1,71 @@
 export type PatronType = {
-    email: string
-    username: string
-    firstname?: string
-    lastname?: string
-    avatar?: string
-    contact?: number | undefined
-    suspended?: boolean
+  email: string
+  username: string
+  firstname?: string
+  lastname?: string
+  avatar?: string
+  contact?: number | undefined
+  suspended?: boolean
 }
 
-export type PatronMDBType = {
-    _id: string; // MongoDB ObjectId as a string
-    id: string; // Campaign ID
-    fullname?: string,
-    firstname?: string,
-    lastname?: string,
-    username: string,
-    email: string,
-    role: string,
-    address?: string,
-    contact?: string,
-    avatar?: string,
-    onlineStatus: boolean,
-    suspended?: boolean,
-    verified: boolean
-    lastSignInTime?: string,
-    createdAt: string; // ISO date string
-    updatedAt: string; // ISO date string
-    __v: number; // Version key
+export type PatronDatabaseType = {
+  id: string
+  username: string
+  fullname?: string
+  firstname?: string
+  lastname?: string
+  email: string
+  contact?: string | null
+  address?: string
+  role: string
+  verified: boolean
+  onlineStatus: boolean
+  avatar?: string
+  createdAt?: string
+  updatedAt?: string
+  lastSignInTime?: string
+  suspended?: boolean
 }
 
 export type PatronWebType = {
-    id: string; // Campaign ID
-    fullname?: string,
-    firstname?: string,
-    lastname?: string,
-    username?: string,
-    email: string,
-    role: string,
-    address?: string,
-    contact?: string,
-    avatar?: string,
-    suspended?: boolean,
-    onlineStatus: boolean,
-    verified: boolean
-    timestamps: {
-        createdAt: string; // ISO date string
-        lastSignInTime?: string,
-    }
-    tokens: {
-        refreshToken: string
-        accessToken: string
-    }
+  id: string // Campaign ID
+  fullname?: string
+  firstname?: string
+  lastname?: string
+  username?: string
+  email: string
+  role: string
+  address?: string
+  contact?: string
+  avatar?: string
+  suspended?: boolean
+  onlineStatus: boolean
+  verified: boolean
+  timestamps: {
+    createdAt: string // ISO date string
+    lastSignInTime?: string
+  }
+  tokens: {
+    refreshToken: string
+    accessToken: string
+  }
 }
 
 export type PatronAdminType = {
-    id: string; // Campaign ID
-    fullname?: string,
-    username?: string,
-    email: string,
-    role: string,
-    address?: string,
-    contact?: string,
-    avatar?: string,
-    suspended?: boolean,
-    onlineStatus: boolean,
-    verified: boolean
-    timestamps: {
-        createdAt: string; // ISO date string
-        updatedAt: string; // ISO date string
-        lastSignInTime: string,
-    }
+  id: string // Campaign ID
+  fullname?: string
+  username?: string
+  email: string
+  role: string
+  address?: string
+  contact?: string
+  avatar?: string
+  suspended?: boolean
+  onlineStatus: boolean
+  verified: boolean
+  timestamps: {
+    createdAt: string // ISO date string
+    updatedAt: string // ISO date string
+    lastSignInTime: string
+  }
 }
