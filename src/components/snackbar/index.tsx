8f1ae20a65c647076879b14bb3a93da3269dfaf8
Samuel Tetteh <khobbylynx55@gmail.com>
2025-03-02 14:30:06 +0000

    Create index.tsx

---
+++ src/components/snackbar/index.tsx
@@ -0,0 +1,57 @@
+// ** React Imports
+import { Fragment, SyntheticEvent } from 'react'
+
+// ** MUI Imports
+import Alert from '@mui/material/Alert'
+import Snackbar from '@mui/material/Snackbar'
+
+interface SnackBarProps {
+  open: boolean
+  handleOpen: () => void
+  message: string
+  variant?: 'standard' | 'filled' | 'outlined'
+  color?: 'success' | 'error' | 'info' | 'warning'
+  vertical?: 'top' | 'bottom'
+  horizontal?: 'left' | 'right' | 'center'
+}
+
+const SnackbarAlert = ({
+  open,
+  handleOpen,
+  color,
+  variant,
+  message,
+  vertical = 'bottom',
+  horizontal = 'left',
+}: SnackBarProps) => {
+  const handleClose = (event?: Event | SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return
+    }
+    handleOpen()
+  }
+
+  return (
+    <Fragment>
+      <Snackbar
+        open={open}
+        onClose={handleClose}
+        autoHideDuration={3000}
+        key={vertical + horizontal}
+        anchorOrigin={{ vertical, horizontal }}
+      >
+        <Alert
+          variant={variant || 'filled'}
+          severity={color || 'info'}
+          onClose={handleClose}
+          sx={{ width: '100%' }}
+          //   elevation={skin === 'bordered' ? 0 : 3}
+        >
+          {message}
+        </Alert>
+      </Snackbar>
+    </Fragment>
+  )
+}
+
+export default SnackbarAlert
