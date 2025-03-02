2c36fb2672b44b62f9f145815d36bf55e4ef92bf
Samuel Tetteh <khobbylynx55@gmail.com>
2025-03-02 00:03:18 +0000

    Update patron.model.ts

--- src/models/patron.model.ts
+++ src/models/patron.model.ts
@@ -1,60 +1,69 @@
 import mongoose, { Schema } from 'mongoose'
 
 const patronSchema = new Schema(
-    {
-        id: {
-            type: String,
-            required: true
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        fullname: {
-            type: String,
-        },
-        firstname: {
-            type: String,
-        },
-        lastname: {
-            type: String
-        },
-        email: {
-            type: String
-        },
-        contact: {
-            type: Number
-        },
-        address: {
-            type: String
-        },
-        role: {
-            type: String,
-            required: true
-        },
-        verified: {
-            type: Boolean,
-            required: true
-        },
-        onlineStatus: {
-            type: Boolean
-        },
-        suspended: {
-            type: Boolean
-        },
-        avatar: {
-            type: String
-        },
-        lastSignInTime: {
-            type: String
-        },
-        password: {
-            type: String
-        }
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
     },
-    {
-        timestamps: true
-    }
+    username: {
+      type: String,
+      required: true,
+    },
+    fullname: {
+      type: String,
+    },
+    firstname: {
+      type: String,
+    },
+    lastname: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    contact: {
+      type: Number,
+    },
+    address: {
+      type: String,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    verified: {
+      type: Boolean,
+      required: true,
+    },
+    onlineStatus: {
+      type: Boolean,
+      required: true,
+    },
+    suspended: {
+      type: Boolean,
+    },
+    type: {
+      type: String,
+    },
+    avatar: {
+      type: String,
+    },
+    lastSignInTime: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
 )
 
 const Patron = mongoose.models.Patron || mongoose.model('Patron', patronSchema)
