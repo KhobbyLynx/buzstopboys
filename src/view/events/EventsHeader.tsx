42b07e7cc774026979e9e6ff9b7465aaeff2206a
Samuel Tetteh <khobbylynx55@gmail.com>
2025-03-04 00:55:10 +0000

    Update EventsHeader.tsx

--- src/view/events/EventsHeader.tsx
+++ src/view/events/EventsHeader.tsx
@@ -1,13 +1,9 @@
-"use client";
-import React from 'react';
+'use client'
+import React from 'react'
 
-import {
-  Typography,
-} from "@material-tailwind/react";
-
+import { Typography } from '@material-tailwind/react'
 
 export function EventsHeader() {
-
   return (
     <section className="mx-auto py-10">
       <div className="mb-10 grid place-items-center text-center">
@@ -15,14 +11,16 @@ export function EventsHeader() {
           Cleaning Ghana, One Step at a Time
         </Typography>
         <Typography variant="lead" className="!text-gray-500 lg:w-6/12">
-        Join us as we take action to keep our communities clean and safe! Basta Boys is dedicated to transforming public spaces through cleanup projects, community engagement, and awareness campaigns. Check out our upcoming events and be part of the change.
+          Join us as we take action to keep our communities clean and safe! BuzStopBoys is dedicated
+          to transforming public spaces through cleanup projects, community engagement, and
+          awareness campaigns. Check out our upcoming events and be part of the change.
         </Typography>
         <Typography variant="h4" className="my-3 text-blue-400">
           You can volunteer to any listed Event
         </Typography>
       </div>
     </section>
-  );
+  )
 }
 
-export default EventsHeader;
+export default EventsHeader
