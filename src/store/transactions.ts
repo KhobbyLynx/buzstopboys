e576ef44072d48b8949e23e649c4f8ab19c956a6
Samuel Tetteh <khobbylynx55@gmail.com>
2025-03-05 02:37:44 +0000

    Update transactions.ts

--- src/store/transactions.ts
+++ src/store/transactions.ts
@@ -4,31 +4,43 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 // ** Axios
 import axiosRequest from '@/utils/axiosRequest'
 import { PAYMENTDATATYPE, TRANSACTIONSTYPE } from '@/types/transactions'
+import { BProgress } from '@bprogress/core'
+import { Toast } from '@/utils/toast'
 
-export const handlePayment = createAsyncThunk(
-  'transactions/handlePayment',
+export const handleNewTransaction = createAsyncThunk(
+  'transactions/handleNewTransaction',
   async (paymentData: PAYMENTDATATYPE, { rejectWithValue }) => {
+    const status = paymentData.status
     try {
-      const response = await axiosRequest.post('/paystack', paymentData)
-      const { paystack } = response.data
+      // Start Progress bar
+      BProgress.start()
 
-      // RESPONSE DATA STRUCTURE
-      // {
-      //   "status": true,
-      //   "message": "Authorization URL created",
-      //    "data": {
-      //      "authorization_url": "https://checkout.paystack.com/abc123xyz",
-      //      "access_code": "abc123xyz",
-      //      "reference": "7PVGX8MEk85tgeEpVDtD"
-      //     }
-      //   }
+      const response = await axiosRequest.post('/transactions', paymentData)
+      const newTransactionData = response.data
 
-      return paystack
+      // End Progress bar
+      BProgress.done()
+      Toast.fire({
+        icon:
+          status === 'success'
+            ? 'success'
+            : status === 'cancelled' || 'falied'
+            ? 'error'
+            : 'warning',
+
+        title: 'Donation',
+        text: `Payment ${status}`,
+      })
+
+      return newTransactionData
     } catch (error) {
       if (process.env.NEXT_PUBLIC_NODE_ENV === 'development') {
         console.log('payment error', error)
       }
       return rejectWithValue(error instanceof Error ? error.message : 'Payment error')
+    } finally {
+      // End Progress bar
+      BProgress.done()
     }
   }
 )
@@ -39,7 +51,6 @@ export const transactionsSlice = createSlice({
     // TRANSACTIONS
     data: [] as TRANSACTIONSTYPE[],
     selectedTransaction: {} as TRANSACTIONSTYPE,
-    initializePayment: {},
 
     // COUNT
     totalTransactions: 0 as number,
@@ -53,9 +64,9 @@ export const transactionsSlice = createSlice({
   reducers: {},
   extraReducers(builder) {
     builder
-      // PAYMENT INITIALIZATION
-      .addCase(handlePayment.fulfilled, (state, action) => {
-        state.initializePayment = action.payload
+      // NEW TRANSACTION
+      .addCase(handleNewTransaction.fulfilled, (state, action) => {
+        state.data.push(action.payload)
       })
   },
 })
