8a3c3fef7ed00b50c35883b1de163b538df4a93c
Samuel Tetteh <khobbylynx55@gmail.com>
2025-03-24 15:12:49 +0000

    Deactivate Account

--- src/app/user/settings/page.tsx
+++ src/app/user/settings/page.tsx
@@ -1,7 +1,58 @@
-import React from 'react'
+'use client'
+import DeactivateAccount from '@/view/user/settings/DeactivateAccount'
+// import DeleteAccount from '@/view/user/settings/deleteAccount'
+
+import { Icon } from '@iconify/react'
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Checkbox,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  Grid,
+  Typography,
+} from '@mui/material'
+import React, { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
 
 const UserSettings = () => {
-  return <div>UserSettings </div>
+  // ** State
+  const [open, setOpen] = useState<boolean>(false)
+  const [userInput, setUserInput] = useState<string>('yes')
+  const [secondDialogOpen, setSecondDialogOpen] = useState<boolean>(false)
+
+  // ** Hooks
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: { checkbox: false } })
+
+  const handleClose = () => setOpen(false)
+
+  const handleSecondDialogClose = () => setSecondDialogOpen(false)
+
+  const handleConfirmation = (value: string) => {
+    handleClose()
+    setUserInput(value)
+    setSecondDialogOpen(true)
+  }
+
+  const onSubmit = () => setOpen(true)
+
+  return (
+    <div>
+      {/* <DeleteAccount /> */}
+      <DeactivateAccount />
+    </div>
+  )
 }
 
 export default UserSettings
