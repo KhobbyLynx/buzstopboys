b6c17f6ce7ba34354da1c51187b145f7cadbe3d3
Samuel Tetteh <khobbylynx55@gmail.com>
2025-04-04 02:57:25 +0000

    About page metadata

--- src/app/(website)/about-us/page.tsx
+++ src/app/(website)/about-us/page.tsx
@@ -1,11 +1,25 @@
-'use client'
-
 // ** COMPONENTS
 import PageHero from '@/view/website/about/AboutHero'
 import OurStats from '@/view/website/about/OurStats'
 import MoreInfo from '@/view/website/about/More'
 import Team from '@/components/team'
 import { Box } from '@mui/material'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'About Us',
+  description:
+    ' BuzStopBoys is a dedicated non-governmental organization based in Accra, Ghana, committed to creating cleaner and healthier environments for all.',
+  keywords: [
+    'BuzStopBoys',
+    'volunteering',
+    'non-governmental organization',
+    'clean environment',
+    'Accra',
+    'Ghana',
+    'community service',
+  ],
+}
 
 function About() {
   return (
