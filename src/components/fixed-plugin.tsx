aaea4e0e17d93d6b33876c88908940fc0fcb78b8
Samuel Tetteh <khobbylynx55@gmail.com>
2025-04-10 04:48:17 +0000

    Update fixed-plugin.tsx

--- src/components/fixed-plugin.tsx
+++ src/components/fixed-plugin.tsx
@@ -1,6 +1,6 @@
-"use client";
-import Image from "next/image";
-import { Button } from "@material-tailwind/react";
+'use client'
+import Image from 'next/image'
+import { Button } from '@material-tailwind/react'
 
 export function FixedPlugin() {
   return (
@@ -8,17 +8,17 @@ export function FixedPlugin() {
       <Button
         color="white"
         size="sm"
-        className="!fixed bottom-4 right-4 flex gap-1 pl-2 items-center border border-blue-gray-50"
+        className="!fixed bottom-4 left-4 flex gap-1 pl-2 items-center border z-[99]"
       >
         <Image
           width={128}
           height={128}
           className="w-5 h-5"
           alt="Lynx"
-          src="https://samueltetteh.netlify.app/static/media/logoDark.aa5b846a8282a664eace.png"
-        />{" "}
-        Made With 💖 by Samuel Tetteh
+          src="/images/partners/lynxnet_black.svg"
+        />{' '}
+        Made With 💖 by Lynxnet Innovations
       </Button>
     </a>
-  );
+  )
 }
